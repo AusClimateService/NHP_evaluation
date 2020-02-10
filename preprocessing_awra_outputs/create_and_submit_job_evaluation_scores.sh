62ee197adc4f2c729d467d51d46bb076e0e2e8b1
Vi Co Duong <vico.duong@bom.gov.au>
2020-02-10 12:06:12 +1100

    Update job template
    - Wrap replaceable strings in template with "xx"
    - This greatly reduces possibility of replacing an unrelated string inadvertently
    - Add "-l storage=" as this is needed on Gadi now
    - Request both gdata and scratch areas for er4 for PBS Job
    - Remove commented out email option
    - Job output and error files are now replaced using template string
    - out_path now per user, and uses scratch area
    - Use vertical bar "|" for sed delimiter to allow forward slashes in the replacement string

--- preprocessing_awra_outputs/create_and_submit_job_evaluation_scores.sh
+++ preprocessing_awra_outputs/create_and_submit_job_evaluation_scores.sh
@@ -13,20 +13,27 @@ STATISTICS=(mean min max std pctl05 pctl10 pctl25 pctl50 pctl75 pctl90 pctl95)
 # Create output PBS Jobs folder if it doesn't exist
 mkdir -p ${PBS_JOBS_FOLDER}
 
-# Generate all JOBS
+
+# Generate and run all jobs
 for var in ${VARIABLES[@]}; do
     for gcm in ${GCMS[@]}; do
         for timescale in ${TIMESCALES[@]}; do
             for statistic in ${STATISTICS[@]}; do
-
-                job_file=${PBS_JOBS_FOLDER}/job_evaluation_scores_awra_output_isimip_data_${var}_${gcm}_${timescale}_${statistic}.pbs
-                cp ${PBS_JOB_TEMPLATE_FILE} ${job_file}
+                job_file_base_name=job_evaluation_scores_awra_output_isimip_data_${var}_${gcm}_${timescale}_${statistic}
+                job_file=${PBS_JOBS_FOLDER}/${job_file_base_name}.pbs
+                job_name=isimip_outputs_${gcm}_${var}_${timescale}_${statistic}_evaluation_scores_awra_output
+                job_output_file=${job_file_base_name}.out
+                job_error_file=${job_file_base_name}.error
                 
-                # replace template text in the job file
-                sed -i "s/VAR/${var}/g" ${job_file}
-                sed -i "s/GCM/${gcm}/g" ${job_file}
-                sed -i "s/TIMESCALE/${timescale}/g" ${job_file}
-                sed -i "s/STATISTIC/${statistic}/g" ${job_file}
+                # Create job file from template
+                cp ${PBS_JOB_TEMPLATE_FILE} ${job_file}
+                sed -i "s|xxVARxx|${var}|g" ${job_file}
+                sed -i "s|xxGCMxx|${gcm}|g" ${job_file}
+                sed -i "s|xxTIMESCALExx|${timescale}|g" ${job_file}
+                sed -i "s|xxSTATISTICxx|${statistic}|g" ${job_file}
+                sed -i "s|xxJOB_NAMExx|${job_name}|g" ${job_file}
+                sed -i "s|xxJOB_OUTPUT_FILExx|${job_output_file}|g" ${job_file}
+                sed -i "s|xxJOB_ERROR_FILExx|${job_error_file}|g" ${job_file}
                 wait
                 
                 # submit job
