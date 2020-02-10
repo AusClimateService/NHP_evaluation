3ec4c495f513d92a880787e3b12a5c71812c4539
Vi Co Duong <vico.duong@bom.gov.au>
2020-02-10 11:12:06 +1100

    Refactor
    - Use lists to see all variables/gcms/timscales/stats at the same level
    - Use a single variable for output job file rather than repeating
    - Create PBS_jobs folder if not exist
    - Remove hardcoded /g/data path
    - Intention is for individual users to have their own copies of scripts

--- preprocessing_awra_outputs/create_and_submit_job_evaluation_scores.sh
+++ preprocessing_awra_outputs/create_and_submit_job_evaluation_scores.sh
@@ -1,30 +1,38 @@
+#!/bin/bash
 
-cd /g/data1a/er4/exv563/hydro_projections/code
+PBS_JOBS_FOLDER=PBS_jobs
+PBS_JOB_TEMPLATE_FILE=job_evaluation_scores_awra_output_isimip_data_TEMPLATE.pbs
 
-for var in sm etot qtot; do
-# for var in sm; do
-	for gcm in GFDL-ESM2M; do
-		# for timescale in year mon; do
-		for timescale in seas; do
-			for statistic in mean min max std pctl05 pctl10 pctl25 pctl50 pctl75 pctl90 pctl95; do
-			#for statistic in pctl05; do
 
-		        # copy the template to a new file
-		        cp postprocessing/ISIMIP_original/awra_outputs/job_evaluation_scores_awra_output_isimip_data_TEMPLATE.pbs PBS_jobs/job_evaluation_scores_awra_output_isimip_data_${var}_${gcm}_${timescale}_${statistic}.pbs 
-		        wait
-		        
-		        # replace VAR in the job file
-		        sed -i "s/VAR/${var}/g" "PBS_jobs/job_evaluation_scores_awra_output_isimip_data_${var}_${gcm}_${timescale}_${statistic}.pbs"
-		        sed -i "s/GCM/${gcm}/g" "PBS_jobs/job_evaluation_scores_awra_output_isimip_data_${var}_${gcm}_${timescale}_${statistic}.pbs"
-		        sed -i "s/TIMESCALE/${timescale}/g" "PBS_jobs/job_evaluation_scores_awra_output_isimip_data_${var}_${gcm}_${timescale}_${statistic}.pbs"
-		        sed -i "s/STATISTIC/${statistic}/g" "PBS_jobs/job_evaluation_scores_awra_output_isimip_data_${var}_${gcm}_${timescale}_${statistic}.pbs"
-		        
-		        wait
-		        
-		        # submit job
-		        qsub PBS_jobs/job_evaluation_scores_awra_output_isimip_data_${var}_${gcm}_${timescale}_${statistic}.pbs
-		        wait
-		    done
-	    done
-	done
+VARIABLES=(sm etot qtot)
+GCMS=(GFDL-ESM2M)
+TIMESCALES=(seas)
+STATISTICS=(mean min max std pctl05 pctl10 pctl25 pctl50 pctl75 pctl90 pctl95)
+
+
+# Create output PBS Jobs folder if it doesn't exist
+mkdir -p ${PBS_JOBS_FOLDER}
+
+# Generate all JOBS
+for var in ${VARIABLES[@]}; do
+    for gcm in ${GCMS[@]}; do
+        for timescale in ${TIMESCALES[@]}; do
+            for statistic in ${STATISTICS[@]}; do
+
+                job_file=${PBS_JOBS_FOLDER}/job_evaluation_scores_awra_output_isimip_data_${var}_${gcm}_${timescale}_${statistic}.pbs
+                cp ${PBS_JOB_TEMPLATE_FILE} ${job_file}
+                
+                # replace template text in the job file
+                sed -i "s/VAR/${var}/g" ${job_file}
+                sed -i "s/GCM/${gcm}/g" ${job_file}
+                sed -i "s/TIMESCALE/${timescale}/g" ${job_file}
+                sed -i "s/STATISTIC/${statistic}/g" ${job_file}
+                wait
+                
+                # submit job
+                qsub ${job_file}
+                wait
+            done
+        done
+    done
 done
