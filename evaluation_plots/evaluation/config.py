2de1284b721bf7b7aec62471a59a193b5c141fdc
Vi Co Duong <vico.duong@bom.gov.au>
2020-03-06 09:39:36 +1100

    Support environment variables in config.json
    - Limited to values that get loaded as string types

--- evaluation_plots/evaluation/config.py
+++ evaluation_plots/evaluation/config.py
@@ -7,4 +7,10 @@ def load_config():
     with open('config.json', 'r') as fp:
         parameters = json.load(fp)
 
+    # Resolve any environment variables in string values
+    for k in parameters.keys():
+        value = parameters[k]
+        if type(value) == str:
+            parameters[k] = os.path.expandvars(value)
+
     return parameters
